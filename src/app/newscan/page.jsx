735cffadeb060d7bab7c142dacfc06b037d30677
Awsome-obedeh <obedchidera1010@gmail.com>
2026-03-27 14:19:51 +0100

    feat:newScan page UI

--- src/app/newscan/page.jsx
+++ src/app/newscan/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-        <h1>This is the new scan</h1>
+        <h1>This is the new scan, welcome to scan</h1>
     </div>
   )
 }
